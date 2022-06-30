0653c73f6e742c55a7882c4cf59d379e93decd63
Stridsvagn69420 <Stridsvagn69420@users.noreply.github.com>
2022-06-30 20:18:04 +0200

    Sample Code

---
+++ README.md
@@ -0,0 +1,8 @@
+# GtkPass
+Pass-compatible Password Manager GUI written with gtk-rs and Glade
+
+# Installing
+WIP.
+
+# Usage
+WIP.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Button};
+
 fn main() {
-    println!("Hello, world!");
+    let application = Application::builder()
+        .application_id("de.Stridsvagn69420.GtkPass")
+        .build();
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("GTK Pass")
+            .default_width(350)
+            .default_height(70)
+            .build();
+
+        let button = Button::with_label("Click me!");
+        button.connect_clicked(|_| {
+            eprintln!("Clicked!");
+        });
+        window.add(&button);
+
+        window.show_all();
+    });
+
+    application.run();
 }
